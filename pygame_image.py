8466fa275784a3ee84ba9f82c63006f3e5995d83
kato yuta <c0b2216770@edu.teu.ac.jp>
2023-11-21 16:18:06 +0900

    演習課題2(背景画像の雲が不連続になっている修正)

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("01/fig/pg_bg.jpg")#練習1
+    bg_imgs = [bg_img,pg.transform.flip(bg_img,True,False)]#演習課題2(背景画像の不連続性の改善)
     kk_img = pg.image.load("01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True, False)#練習2
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]#練習3
@@ -15,8 +16,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x = tmr % 1600
-        screen.blit(bg_img, [-x, 0])#練習4
-        screen.blit(bg_img, [1600-x,0])
+        screen.blit(bg_imgs[0], [-x, 0])#練習4
+        screen.blit(bg_imgs[1], [1600-x,0])#練習6
         screen.blit(kk_imgs[tmr%2], [300,200])#練習5
         pg.display.update()
         tmr += 1        
